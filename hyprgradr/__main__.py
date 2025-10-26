04c896b594ff6aaed028763fb1fb87448bf09ce1
Joseph DePalo <jdepalo@proton.me>
2025-10-26 15:35:28 -0400

    Switched to using TOML config instead of CLI for common options.

--- hyprgradr/__main__.py
+++ hyprgradr/__main__.py
@@ -1,6 +1,9 @@
 import argparse
+import tomllib
 
-# from hyprgradr import subcmd_handlers as hndl
+from pydantic import BaseModel, FilePath
+from typing import Optional
+
 from . import subcmd_handlers as hndl
 
 
@@ -8,12 +11,10 @@ def build_parser():
     parser = argparse.ArgumentParser(
         description="A CLI utility for interacting with the Canvas LMS API.1"
     )
-    parser.add_argument("-c", "--courseid", required=True)
-    parser.add_argument("-a", "--assignmentid", required=True)
-    parser.add_argument("-t", "--tokenfile", required=True)
+    parser.add_argument("--config", required=True)
     student_group = parser.add_mutually_exclusive_group(required=True)
-    student_group.add_argument("-s", "--studentid")
-    student_group.add_argument("-n", "--studentname")
+    student_group.add_argument("-s", "--student-id")
+    student_group.add_argument("-n", "--student-name")
 
     subparsers = parser.add_subparsers(dest="command", required=True)
 
@@ -25,10 +26,28 @@ def build_parser():
     return parser
 
 
+class Config(BaseModel):
+    course_id: int
+    assignment_id: int
+    token_file: FilePath
+    eb_days: int
+    base_url: str
+
+    @classmethod
+    def from_args(cls, args: argparse.Namespace) -> "Config":
+        config = {}
+        if args.config:
+            with open(args.config, "rb") as f:
+                config = tomllib.load(f)
+
+        return cls(**config)
+
+
 def main():
     args = build_parser().parse_args()
+    config = Config.from_args(args)
 
-    args.func(args)
+    args.func(args, config)
 
 
 if __name__ == "__main__":
--- hyprgradr/subcmd_handlers.py
+++ hyprgradr/subcmd_handlers.py
@@ -1,8 +1,9 @@
 from .submission import Submission
 
 
-def download_submission(args):
-    api_token = open(args.tokenfile).read().strip()
-    submission = Submission(args.courseid, args.assignmentid, args.studentid, api_token)
-    print(args)
+def download_submission(args, config):
+    api_token = open(config.token_file).read().strip()
+    submission = Submission(
+        config.course_id, config.assignment_id, args.student_id, api_token
+    )
     submission.get_student_submissions()
